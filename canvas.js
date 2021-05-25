f0cce860f548b6cc0d0910b46c30a5eea6e0ef97
EddyBautista <bautista.j.eddy@gmail.com>
2021-05-24 19:13:20 -0500

    movement

--- canvas.js
+++ canvas.js
@@ -1,33 +1,18 @@
-console.log('Hello World!!!');
-
 var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-// HTMLCanvasElement.getContext(contextId: "2d", options?: CanvasRenderingContext2DSettings)
-// : CanvasRenderingContext2D
-// The identifier (ID) of the type of canvas to create. Internet Explorer 9
-// and Internet Explorer 10 support only a 2-D context using
-// canvas.getContext("2d"); IE11 Preview also supports 3-D or
-// WebGL context using canvas.getContext("experimental-webgl");
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}             
 
-
-// Returns an object that provides methods and properties for drawing
-// and manipulating images and graphics on a canvas element in a document.
-// A context object includes information about colors, line widths, fonts,
-// and other graphic parameters that can be drawn on a canvas.
-
-var ctx = canvas.getContext("2d")
-
-// Example code to create a red square 
-
-ctx.beginPath(); // creates a new path by emptying the subpath 
-ctx.rect(20, 40, 50, 50); // defines a rectangle
-ctx.fillStyle = "#FF0000"; // color 
-ctx.fill();
-ctx.closePath(); // The CanvasRenderingContext2D.closePath() method of the 
-                 // Canvas 2D API attempts to add a straight line from the current point to the start of the current sub-path. If the shape has already been closed or has only one point, this function does nothing.
-
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+setInterval(draw, 10);
